8cfc9cb30a887a11dcf040490e4aae47315c0803
GordonFreemanK <evren.kuzucuoglu@gmail.com>
2022-01-20 10:09:59 +0000

    Add Install-PSDigiKam function

--- GFK.Image/Scripts/GFK.Image.digiKam.psm1
+++ GFK.Image/Scripts/GFK.Image.digiKam.psm1
@@ -1,4 +1,36 @@
-#Requires -Module GFK.Image.PowerShell
+#Requires -Module GFK.Image
+
+<#
+.SYNOPSIS
+    Injects PowerShell into digiKam
+.DESCRIPTION
+.NOTE
+    digiKam is expected to be installed for this function to work
+#>
+
+[CmdletBinding]
+function Install-PSDigiKam {
+    Write-Output $PSScriptRoot
+    Write-Output (Get-Location).Path
+    
+    if ($env:OS -ne 'Windows_NT' -or $env:PROCESSOR_ARCHITECTURE -ne 'AMD64') {
+        throw 'This function is only compatible with win64 systems'
+    }
+
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal $identity
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+        throw 'This function must run in an elevated shell'
+    }
+
+    $digiKamRegistryKey = Get-Item HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\digiKam -ErrorAction SilentlyContinue
+    if (-not $digiKamRegistryKey) {
+        throw 'digiKam does not appear to be installed'
+    }
+
+    $digiKamInstallLocation = $digiKamRegistryKey.GetValue('InstallLocation')
+    Copy-Item 'cmd/cmd.exe' $digiKamInstallLocation
+}
 
 <#
 .SYNOPSIS
@@ -8,8 +40,7 @@
     This will load these tags in a new PowerShell drive called Tag:/ to enable exploring them
 #>
 [CmdletBinding]
-function New-TagsDrive
-{
+function New-TagsDrive {
     param ([Parameter(Mandatory)][string]$Tags)
 
     New-PSDrive -Name Tags -PSProvider Tags -Root 'Tags:' -Scope Global | Out-Null
