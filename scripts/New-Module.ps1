ebb20b75b618ef4443b63db4e9206fb61af8f0f5
GordonFreemanK <evren.kuzucuoglu@gmail.com>
2022-01-28 14:53:15 +0000

    Improvement on scripts

---
+++ scripts/New-Module.ps1
@@ -0,0 +1,10 @@
+﻿#Requires -PSEdition Core
+
+param(
+    [Parameter(Mandatory, Position=0)][string] $Path,
+    [Parameter()][string] $Configuration = 'Debug'
+)
+
+$ModulePath = Join-Path $Path GFK.Image;
+dotnet publish GFK.Image -c $Configuration -o $ModulePath --nologo;
+dotnet publish GFK.Image.cmd -c $Configuration -o (Join-Path $ModulePath cmd) --nologo;
